e4ad1ec8bbfed41d3e3b5fb7d425d33648a6d781
bubblestack <dgokadze@gmail.com>
2016-08-14 02:13:42 +0400

    Update eth2relay.ino

--- eth2relay.ino
+++ eth2relay.ino
@@ -21,9 +21,9 @@ boolean reading = false;
 
 
 
-int SER_Pin = 6;   //pin 14 on the 75HC595
-int RCLK_Pin = 7;  //pin 12 on the 75HC595
-int SRCLK_Pin = 5; //pin 11 on the 75HC595
+int SER_Pin = 6;   //pin 14 on the 75HC595  ------------- BLUE
+int RCLK_Pin = 7;  //pin 12 on the 75HC595  -------------  GREEN
+int SRCLK_Pin = 5; //pin 11 on the 75HC595 ------------ YELLOW
 
 //How many of the shift registers - change this
 #define number_of_74hc595s 2
